cf682343e75240fbea0a4b1e3c04694a5aba2d4d
Andrew <andrw.hnskr@gmail.com>
2014-12-03 15:25:45 -0700

    set up devise

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ end
 
 gem 'bootstrap-sass', '~> 3.1.1'
 
+gem 'devise'
+gem 'figaro', '1.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blocipedia::Application.config.secret_key_base = '9983f112e0a7ae851ef459136813c12653263150f835c2827430e6775601148a70bd0810470042f73666648bb0ecd4191616ed1a83d3e0df6de0d99f9ee98dc2'
+Blocipedia::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
